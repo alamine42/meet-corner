77022950ae1f35f932605a52332e677848116aea
alamine42 <alamine@gmail.com>
2015-08-02 19:44:28 -0400

    Added code to render templates and connect to DB

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from __future__ import print_function
-from flask import Flask
+from flask import Flask, render_template, request, session, \
+flash, redirect, url_for, g
+import sqlite3
+
+DATABASE = 'meet_corner.db'
 
 app = Flask('__name__')
-app.config['DEBUG'] = True
+
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE_PATH'])
 
 @app.route('/')
-def landing_page():
-    return 'This is a test of the emergency broadcast system.'
+@app.route('/calendar')
+def calendar_view():
+    return render_template('calendar.html')
+
+@app.route('/meet/<int:meet_id>/')
+def single_meet_view(meet_id):
+    if meet_id > 0:
+        return render_template('meet.html')
+    else:
+        return 'What you talking bout, Willis?', 404
+
 
 if __name__ == '__main__':
-    app.run(port = 5001)
+    app.run(port = 5001, debug = True)
